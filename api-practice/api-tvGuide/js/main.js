42d21c1eebb143a272bf4b12965138547bcdb27e
unknown <tehuberjohn@hotmail.co.uk>
2022-04-14 12:06:48 -0500

    plugging in the last of the api info, just needs formatting

--- api-practice/api-tvGuide/js/main.js
+++ api-practice/api-tvGuide/js/main.js
@@ -1,8 +1,6 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
-function arrayToString(arr){
-  arr.join(', ')
-}
+
 function getFetch(){
   const choice = document.querySelector('input').value
   const url =  `https://api.tvmaze.com/search/shows?q=${choice}`
@@ -13,14 +11,18 @@ function getFetch(){
         let show = data[0].show
         let score = show.rating.average ? `Rating: ${show.rating.average} / 10` : 'Rating: unrated'
         let genre = show.genres.join(', ')
+        let showEnd = show.ended ? show.ended : 'present'
+        let runningStats = `Original release: ${show.premiered} - ${showEnd}`
         console.log(show.genres)
         document.querySelector('img').src = show.image.original
         document.querySelector('h2').innerText = show.name
         document.getElementById('summary').innerHTML = String(show.summary.replaceAll('<p>','').replace('</p>',''))
         document.getElementById('language').innerText = `language: ${show.language}`
         document.getElementById('site').innerText = `website: ${show.officialSite}`
-        document.getElementById('score').innerText = score
+        document.getElementById('score').innerText = `${score} | Episode Length: ${show.averageRuntime} mins`
         document.getElementById('genre').innerText = `genre: ${genre}`
+        document.getElementById('runningStats').innerText = runningStats
+
       })
       .catch(err => {
         console.log(`error ${err}`)
